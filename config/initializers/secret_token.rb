9c6d9faf8006cb96f4254adb5582fb5d2f76acad
Dmitriy Kiriyenko <dmitriy.kiriyenko@anahoret.com>
2012-05-09 13:19:45 +0300

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyNextProject::Application.config.secret_token = '898f2c35f995009c989ee6835eba5d38c84c61df592d49b88cf2c0bf3f1b47e3d3f17cf19ac663e74cd00dad4e261b462ea92b73de85bb5656a604695c9273de'
